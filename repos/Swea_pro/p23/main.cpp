0472d72e54eae01112c98c047a19b3d431c41db2
taegun1011 <xorjsghkd1011@gmail.com>
2024-08-20 23:01:23 +0900

    0820 solved

--- repos/Swea_pro/p23/main.cpp
+++ repos/Swea_pro/p23/main.cpp
@@ -83,7 +83,7 @@ static bool run()
 int main()
 {
 	setbuf(stdout, NULL);
-	// freopen("sample_input.txt", "r", stdin);
+	freopen("sample_input.txt", "r", stdin);
 	int T, MARK;
 	scanf("%d %d", &T, &MARK);
 
