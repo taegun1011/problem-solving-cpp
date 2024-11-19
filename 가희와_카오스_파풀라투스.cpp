94ea1faa84888aa7e7227e4d62f1a525c6774aad
taegun1011 <xorjsghkd1011@gmail.com>
2024-11-19 17:51:13 +0900

    1119 solved

--- 가희와_카오스_파풀라투스.cpp
+++ 가희와_카오스_파풀라투스.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
@@ -9,7 +9,7 @@ using namespace std;
 int N, M;
 
 void solve() {
-    pair
+    
 }
 
 int main() {
